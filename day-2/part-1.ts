7247c53a5ae50401836bd6f07aee2eaa33033ff8
Nuriddin <nmuhammadjanov@gmail.com>
2024-12-02 22:08:05 +0500

    day 2, both parts

---
+++ day-2/part-1.ts
@@ -0,0 +1,56 @@
+import fs from 'fs'
+import path from 'path'
+
+const input = fs.readFileSync(path.join(__dirname, "./input.txt"), "utf8")
+
+function check_levels(levels: number[]) {
+    let direction = 0
+
+    for (let i = 1; i < levels.length; i++) {
+        const level = levels[i],
+            prev_level = levels[i - 1]
+
+        if (level == prev_level) {
+            return false
+        }
+
+        let diff = level - prev_level
+
+        if (direction && Math.sign(direction) != Math.sign(diff)) {
+            return false
+        }
+
+        if (!direction) {
+            direction = Math.sign(diff)
+        }
+
+        if (Math.abs(diff) < 1 ||
+            Math.abs(diff) > 3) {
+            return false
+        }
+    }
+
+    return true
+
+}
+
+function find_number_of_safe_reports(input: string) {
+    let safe_reports_count = 0
+
+    const reports = input.trim().split("\n")
+
+    for (let report of reports) {
+        const levels = report.split(" ").map((value) => +value).filter((value) => !Number.isNaN(value))
+
+        let is_safe = check_levels(levels)
+
+        if (is_safe) {
+            safe_reports_count++
+        }
+    }
+
+    return safe_reports_count
+}
+
+const result = find_number_of_safe_reports(input)
+console.log(result)
